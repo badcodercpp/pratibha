3bebf2ff5b147682f68912da0b3d7ba463aefd03
ajay <ajaykrjha93@gmail.com>
2018-12-24 15:12:05 +0530

    changes made for gathering static method to the current context

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/creat
 
 var _possibleConstructorReturn2 = _interopRequireDefault(require("@babel/runtime/helpers/possibleConstructorReturn"));
 
-var _getPrototypeOf3 = _interopRequireDefault(require("@babel/runtime/helpers/getPrototypeOf"));
+var _getPrototypeOf2 = _interopRequireDefault(require("@babel/runtime/helpers/getPrototypeOf"));
 
 var _inherits2 = _interopRequireDefault(require("@babel/runtime/helpers/inherits"));
 
@@ -53,23 +53,19 @@ var App = (_dec = (0, _index.Link)(), _dec2 = (0, _index.P)(), _dec3 = (0, _inde
 function (_Component) {
   (0, _inherits2.default)(App, _Component);
 
-  function App() {
-    var _getPrototypeOf2;
-
+  function App(props) {
     var _this;
 
     (0, _classCallCheck2.default)(this, App);
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    _this = (0, _possibleConstructorReturn2.default)(this, (_getPrototypeOf2 = (0, _getPrototypeOf3.default)(App)).call.apply(_getPrototypeOf2, [this].concat(args)));
+    _this = (0, _possibleConstructorReturn2.default)(this, (0, _getPrototypeOf2.default)(App).call(this, props));
     (0, _defineProperty2.default)((0, _assertThisInitialized2.default)((0, _assertThisInitialized2.default)(_this)), "_click_p", function (e) {
       e.preventDefault();
       e.stopPropagation();
       alert("hi");
     });
+    _this.Link = _this.constructor.Link;
+    _this.Div = _this.constructor.Div;
+    _this.P = _this.constructor.P;
     return _this;
   }
 
@@ -80,33 +76,33 @@ function (_Component) {
       console.log(this.constructor.P);
       return _react.default.createElement("div", {
         className: "App"
-      }, this.constructor.isTestable, this.constructor.Link({
+      }, this.Link({
         href: "/ajay_jha",
         style: {
           color: "red"
         }
-      }, "click me"), this.constructor.Link({
+      }, "click me"), this.Link({
         href: "/haha",
         style: {
           color: "blue"
         }
-      }, "click me different action"), this.constructor.P({
+      }, "click me different action"), this.P({
         style: {
           color: "green"
         },
         onClick: this._click_p
-      }, "clickable paragraph"), this.constructor.Div({
+      }, "clickable paragraph"), this.Div({
         style: {
           color: "green"
         },
         onClick: this._click_p
-      }, [this.constructor.Link({
+      }, [this.Link({
         href: "/ajay_jha",
         style: {
           color: "yellow"
         },
         key: 0
-      }, "clickable as children"), this.constructor.P({
+      }, "clickable as children"), this.P({
         style: {
           color: "green"
         },
